510183d6d0f171d0592d77e1635ce0ecf192f3df
Florian Barthel <florianbarthel@yahoo.de>
2025-09-21 10:22:05 +0200

    fix pti

--- inversion.py
+++ inversion.py
@@ -7,7 +7,6 @@ from losses.id_loss import IDLoss
 class Inversion:
     def __init__(self):
         self.device = torch.device("cuda")
-        self.generator = None
         self.images = None
         self.cams = None
         self.optimizer = None
@@ -20,11 +19,10 @@ class Inversion:
         self.cams = torch.tensor(cams, device=self.device, dtype=torch.float32)
 
     def set_generator(self, generator):
-        self.generator = generator
-        w_samples = self.generator.mapping(torch.randn(100_000, 512, device=self.device), c=torch.zeros(100_000, 25, device=self.device))
+        w_samples = generator.mapping(torch.randn(100_000, 512, device=self.device), c=torch.zeros(100_000, 25, device=self.device))
         self.w_opt = torch.mean(w_samples, dim=0, keepdim=True).clone().detach().requires_grad_(True)
         self.optimizer_w = torch.optim.Adam(params=[self.w_opt], lr=0.0)
-        self.optimizer_pti = torch.optim.Adam(params=self.generator.parameters(), lr=0.0)
+        self.optimizer_pti = torch.optim.Adam(params=generator.parameters(), lr=0.0)
 
     def calc_loss(self, gen_image, gt_image, weights):
         l2 = self.loss_L2(gen_image, gt_image)
@@ -33,8 +31,8 @@ class Inversion:
         full = l2 * weights["mse_loss"] + lpips * weights["lpips_loss"] + id * weights["id_loss"]
         return full
 
-    def _step(self, hyperparams, optimizer):
-        self.optimizer_w.param_groups[0]["lr"] = hyperparams["lr"]
+    def _step(self, generator, hyperparams, optimizer):
+        optimizer.param_groups[0]["lr"] = hyperparams["lr"]
         batch_size = int(hyperparams["batch_size"])
         optimizer.zero_grad()
         select_indices = np.arange(len(self.images), dtype=int)
@@ -42,16 +40,16 @@ class Inversion:
             select_indices = np.random.choice(select_indices, batch_size)
         ground_truth = self.images[select_indices]
         cam = self.cams[select_indices]
-        generated_images = self.generator.synthesis(ws=self.w_opt.tile(len(self.images), 1), c=cam, random_bg=False)["image"]
+        generated_images = generator.synthesis(ws=self.w_opt.tile(batch_size, 1, 1), c=cam, random_bg=False)["image"]
         loss = self.calc_loss(generated_images, ground_truth, hyperparams)
         loss.backward()
         optimizer.step()
         return loss
 
-    def step_w(self, hyperparams):
-        loss = self._step(hyperparams, self.optimizer_w)
+    def step_w(self, generator, hyperparams):
+        loss = self._step(generator, hyperparams, self.optimizer_w)
         return self.w_opt, loss
 
-    def step_pti(self, hyperparams):
-        loss = self._step(hyperparams, self.optimizer_pti)
+    def step_pti(self, generator, hyperparams):
+        loss = self._step(generator, hyperparams, self.optimizer_pti)
         return self.w_opt, loss
